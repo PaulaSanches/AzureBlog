3d2453030c289c003a0803bc5f26846c9f155cb3
Paula Sanches <paula.correabarrientos@gmail.com>
2023-02-24 15:59:11 +1300

    admin.registered posts.

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
 
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    posts = Post.objects.all()
+    return render(request, 'core/frontpage.html',{'posts':posts})
 
 def about(request):
     return render(request, 'core/about.html')
