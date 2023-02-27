cef9fc57a4cc69e40cc5292f3be06c5f70f5c94a
Paula Sanches <paula.correabarrientos@gmail.com>
2023-02-27 13:21:47 +1300

    Method Post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('<slug:slug>/', views.detail, name='post_detail')
+    path('<slug:slug>/', views.detail, name='post_detail'),
 ]
